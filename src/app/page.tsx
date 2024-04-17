fe802f52bca93a8d4016f5704248cc100fde6fce
abhishek <abhishekbahukhandi2020@gmail.com>
2024-04-17 10:04:44 +0530

    done with basics of next and amazing aceternity ready to rock ui world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection"; 
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyUs from "@/components/Whyus";
+import TestimonialCard from "@/components/ui/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructor from "@/components/Instructor";
+import Footer from  "@/components/Footer"
 
 export default function Home() {
   return (
-    <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.9]">
+    <main className=" min-h-screen bg-black/[0.96] antialiased ">
   
       <HeroSection /> 
+      <FeaturedCourses />
+      <WhyUs />
+      <TestimonialCard />
+      <UpcomingWebinars/>
+      <Instructor />
+        <Footer/>
     </main>
   );
 }
